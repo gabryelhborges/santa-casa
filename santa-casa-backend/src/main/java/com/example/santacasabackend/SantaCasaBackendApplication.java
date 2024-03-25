de77c58c4c9a6b1312cb5f6dbf5a9c6a37a9693e
byelBorges <gabryelhenborges@gmail.com>
2024-03-25 08:37:39 -0300

    inicio projeto

---
+++ santa-casa-backend/src/main/java/com/example/santacasabackend/SantaCasaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.santacasabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaCasaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SantaCasaBackendApplication.class, args);
+    }
+
+}
---
+++ santa-casa-backend/src/test/java/com/example/santacasabackend/SantaCasaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.santacasabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaCasaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
