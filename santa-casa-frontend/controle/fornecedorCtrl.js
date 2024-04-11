5c747f3df417069fcfdd2cf7f811b8574ee34624
LeonRonchi <leonbronchi@gmail.com>
2024-04-10 22:43:48 -0300

    cadastrar fornecedor ok

--- santa-casa-frontend/controle/fornecedorCtrl.js
+++ santa-casa-frontend/controle/fornecedorCtrl.js
@@ -1,7 +1,7 @@
 const urlBase = 'http://localhost:4040/fornecedor';
 
 var formFor = document.getElementById('formFornecedor');
-formFor.requestFullscreen();
+formFor.reset();
 formFor.onsubmit=validarFormulario;
 
 exibirFornecedores();
@@ -92,7 +92,7 @@ function validarFormulario(eventos) {
                 }).then((dados)=>{
                     limparFormulario();
                     exibirMensagem(dados.mensagem);
-                    exibirPacientes();
+                    exibirFornecedores();
                 }).catch((erro)=>{
                     exibirMensagem('Erro ao tentar excluir o fornecedor: '+erro.message);
                 });
@@ -115,7 +115,7 @@ function exibirFornecedores(){
     }).then((json)=>{
         let divTabela = document.getElementById('tabela');
         divTabela.innerHTML = '';
-        listaFornecedores = json.listaFornecedores;
+        listaFornecedores = json.listaFornecedor;
         if(Array.isArray(listaFornecedores)){
             if(listaFornecedores.length > 0){
                 let tabela = document.createElement('table');
@@ -127,6 +127,7 @@ function exibirFornecedores(){
                     <th>CNPJ</th>
                     <th>Nome</th>
                     <th>Telefone</th>
+                </tr>
                 `;
                 tabela.appendChild(cabecalho);
                 let corpo = document.createElement('tbody');
@@ -139,13 +140,13 @@ function exibirFornecedores(){
                         <td>${fornecedor.f_nome}</td>
                         <td>${fornecedor.telefone}</td>
                         <td>
-                            <button class="btn btn-danger" onclick="selecionarPaciente(${gerarParametrosPaciente(paciente)},'excluir')">
+                            <button class="btn btn-danger" onclick="selecionarFornecedor(${gerarParametrosFornecedor(fornecedor)},'excluir')">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
                                     <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
                                 </svg>
                             </button>
-                            <button class="btn btn-warning" onclick="selecionarPaciente(${gerarParametrosPaciente(paciente)},'alterar')">
+                            <button class="btn btn-warning" onclick="selecionarFornecedor(${gerarParametrosFornecedor(fornecedor)},'alterar')">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                                 </svg>
@@ -159,20 +160,20 @@ function exibirFornecedores(){
             }
             else{
                 divTabela.innerHTML = `<div class="alert alert-warning" role="alert"> 
-                                    Não existem fornecedores cadastrados
-                                </div>`;
+                                        Não existem fornecedores cadastrados
+                                        </div>`;
             }
         }
-    }).catch((err0)=>{
+    }).catch((erro)=>{
         exibirMensagem('Não foi possível recuperar os fornecedores do backend: '+erro.message);
     })
 }
 
 function gerarParametrosFornecedor(fornecedor) {
-    return `'${fornecedor.idFornecedor}',${fornecedor.cnpj}',${fornecedor.f_nome}',
-    ${fornecedor.endereco}',${fornecedor.numero}',${fornecedor.complemento}',
-    ${fornecedor.bairro}',${fornecedor.cidade}',
-    ${fornecedor.uf}',${fornecedor.telefone}'`
+    return `'${fornecedor.idFornecedor}','${fornecedor.cnpj}','${fornecedor.f_nome}',
+    '${fornecedor.endereco}','${fornecedor.numero}','${fornecedor.complemento}',
+    '${fornecedor.bairro}','${fornecedor.cidade}',
+    '${fornecedor.uf}','${fornecedor.telefone}'`;
 }
 
 function selecionarFornecedor(idFornecedor,cnpj,f_nome,endereco,numero,complemento,bairro,cidade,uf,telefone, modo){
