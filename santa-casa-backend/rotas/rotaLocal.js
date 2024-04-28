39b43b5bf6a618a233cbecdbd0ef115230d014e2
LeonRonchi <leonbronchi@gmail.com>
2024-04-28 19:05:41 -0300

    rotas dos locais

--- santa-casa-backend/rotas/rotaLocal.js
+++ santa-casa-backend/rotas/rotaLocal.js
@@ -4,3 +4,11 @@ import localCtrl from "../controle/localCtrl.js";
 const locCtrl = new localCtrl();
 const rotaLocal = new Router();
 
+rotaLocal.get('/', locCtrl.consultar)
+.get('/:termo',locCtrl.consultar)
+.post('/',locCtrl.gravar)
+.patch('/',locCtrl.atualizar)
+.put('/',locCtrl.atualizar)
+.delete('/',locCtrl.excluir);
+
+export default rotaLocal;
