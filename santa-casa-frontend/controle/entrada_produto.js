d089a9522f3e6de13a92ec0bf0b5633fde499a7e
YagoAkio <yagokuniyoshi1@gmail.com>
2024-04-30 14:44:30 -0300

    frontend SEMIPRONTO

--- santa-casa-frontend/controle/entrada_produto.js
+++ santa-casa-frontend/controle/entrada_produto.js
@@ -9,20 +9,10 @@ pesquisa.onsubmit = validarFormulario;
 
 var uni = document.getElementById("unidade");
 
-// var entradaProd = document.getElementById('entrada_produto');
-// entradaProd.reset();
-// entradaProd.onsubmit = validarEntrada;
-
-// function validarEntrada(evento){
-//     if(entradaProd.checkValidity()){
-        
-//     }
-//     evento.preventDefault();
-//     evento.stopPropagation();
-// }
 
 
-function adicinar(evento){
+
+function adicinar(){
     fetch(urlBase + "/unidade",{
         method: "GET",
         redirect: "follow"
@@ -49,7 +39,7 @@ function adicinar(evento){
         listaFormaFaramaceuticas = json.listaFormaFaramaceuticas;
         if(Array.isArray(listaFormaFaramaceuticas)){
             for (let i=0 ;i < listaFormaFaramaceuticas.length; i++){  
-                let option = document.createElement("option")  ;
+                let option = document.createElement("option");
                 option.value = listaFormaFaramaceuticas[i].ffa_cod;
                 option.text = listaFormaFaramaceuticas[i].forma;
                 select.appendChild(option);                      
@@ -177,6 +167,7 @@ function selecionarProduto(prod_ID, Fabricante_idFabricante, nome, psicotropico,
         listaFabricante = json.listaFabricante;
         document.getElementById('nome_fabricante').value = listaFabricante[0].f_nome;
     });
+    excluirLote();
     adicionarLote(prod_ID);
 }
 
@@ -188,13 +179,18 @@ function adicionarLote(produto){
     })
     .then((json) => {
         let select = document.getElementById("codigo_lote");
+        let op = document.createElement("option");
+        op.text = "Selecione um Lote";
+        select.appendChild(op);
         listaLotes = json.listaLotes;
         if(Array.isArray(listaLotes)){
             for (let i=0 ;i < listaLotes.length; i++){  
                 let lit = listaLotes[i]
                 let option = document.createElement("option");
                 option.value = lit.codigo + "/" + lit.data_validade
-                 + "/" + lit.conteudo_frasco;
+                 + "/" + lit.conteudo_frasco + "/" + lit.formaFarmaceutica.ffa_cod
+                 + "/" + lit.unidade.un_cod  + "/" + lit.quantidade + 
+                 "/" + lit.total_conteudo; 
                 option.text = lit.codigo + "/" + produto;
                 select.appendChild(option);                      
             };
@@ -213,21 +209,52 @@ function separarPorHifen(str) {
         return {
             parte1: partes[0],
             parte2: partes[1],
-            parte3: partes[2]
+            parte3: partes[2],
+            parte4: partes[3],
+            parte5: partes[4],
+            parte6: partes[5],
+            parte7: partes[6]
         };
     } else {
         return null; // ou throw new Error('A string não contém um hífen.');
     }
 }
 
+function disabled_able() {
+    let a = document.getElementById('capacidade').disabled;
+    let inputs = ['capacidade', 'validade', 'forma_farmaceutica', 'unidade'];
+    
+    for (let i = 0; i < inputs.length; i++) {
+        let input = document.getElementById(inputs[i]);
+        input.disabled = !a;
+    }
+
+}
+
+function criarLimpar(){
+    document.getElementById('capacidade').value = "";
+    document.getElementById('validade').value = "";
+    document.getElementById('forma_farmaceutica').value = "";
+    document.getElementById('unidade').value = "";
+}
+
 document.getElementById("codigo_lote").addEventListener("change", function() {
+    if(document.getElementById('validade').value)
+        disabled_able();
+    criarLimpar();
     if (this.value === "criarNovo") { // Se a opção "Criar Novo Lote" for selecionada
-        criarLote(); // Chama a função criarLote
+        
+        criarLote();
+    }else if(!this.value){
+        criarLimpar();
     }else{
         let valor = separarPorHifen(this.value);
         let capacidadeInteiro = parseInt(valor.parte3); // Acessa a propriedade corretamente
         document.getElementById('capacidade').value = capacidadeInteiro;
-        document.getElementById('validade').value = valor.parte2;
+        document.getElementById('validade').value = formataData(valor.parte2);
+        document.getElementById('forma_farmaceutica').value = valor.parte4;
+        document.getElementById('unidade').value = valor.parte5;
+        disabled_able();
     }
 });
 
@@ -246,4 +273,115 @@ function exibirMensagem(mensagem) {
     setTimeout(() => {
         elemMensagem.innerHTML = '';
     }, 7000);//7 Segundos
+}
+
+function excluirLote(){
+    var select = document.getElementById('codigo_lote');
+    if (select) {
+        while (select.options.length > 0) {
+            select.remove(0);
+        }
+    }
+}
+
+function limparFormulario(){
+    document.getElementById('capacidade').value = "";
+    document.getElementById('validade').value = "";
+    document.getElementById('forma_farmaceutica').value = "";
+    document.getElementById('unidade').value = "";
+    document.getElementById('id_produto').value = "";
+    document.getElementById('nome_produto').value = "";
+    document.getElementById('nome_fabricante').value = "";
+    document.getElementById('codigo_lote').value = "";
+    document.getElementById('quantidade').value = "";
+
+    excluirLote();
+}
+
+document.getElementById('id_produto').addEventListener("change",function(){
+    if(!this.value){
+        if(document.getElementById('codigo_lote').value)
+            disabled_able();
+    
+        limparFormulario();
+    }
+        
+})
+
+var entrada_prod = document.getElementById('form_entrada');
+entrada_prod.reset();
+entrada_prod.onsubmit = validarFormularioEntrada;
+
+function validarFormularioEntrada(evento){
+
+    if(entrada_prod.checkValidity()){
+        let valor = null;
+        if(document.getElementById('validade').disabled == true){
+            valor = separarPorHifen(document.getElementById('codigo_lote').value);
+            let qtd  = parseInt(document.getElementById('quantidade').value) + parseInt(valor.parte6);
+            let prod = new Produto(document.getElementById('id_produto').value);
+            let forma = new FormaFarmaceutica(valor.parte4);
+            let uni = new Unidade(valor.parte5);
+            let tot = parseInt(valor.parte7) + (parseInt(document.getElementById('quantidade').value)*parseInt(valor.parte3));
+            let lote = new Lote(
+                valor.parte1,
+                formataData(valor.parte2),
+                qtd,
+                prod,
+                forma,
+                valor.parte3,
+                uni,
+                tot
+            );
+            fetch(urlBase + "/lote",{
+                method: "PUT",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(lote)
+            }).then((resposta) => {
+                return resposta.json();
+            }).then((dados) => {
+                if (dados.mensagem) {
+                    limparFormulario();
+                    exibirMensagem(dados.mensagem);
+                }
+                else {
+                    exibirMensagem(dados.mensagem);
+                }
+            })
+            .catch((erro) => {
+                exibirMensagem('Não foi possível realizar a operação: ' + erro.message);
+            });
+        }else{
+            let lote = new Lote(
+                document.getElementById().value,
+            );
+            fetch(urlBase + "/lote",{
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(lote)
+            }).then((resposta) => {
+                return resposta.json();
+            }).then((dados) => {
+                if (dados.mensagem) {
+                    limparFormulario();
+                    exibirMensagem(dados.mensagem);
+                }
+                else {
+                    exibirMensagem(dados.mensagem);
+                }
+            })
+            .catch((erro) => {
+                exibirMensagem('Não foi possível realizar a operação: ' + erro.message);
+            });
+        }
+
+    }else{
+        formFor.classList.add('was-validated');
+    }
+    evento.preventDefault();
+    evento.stopPropagation();
 }
