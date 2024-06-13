e123894b86fd7f0163848fe33f7cf7f12bcbb50a
YagoAkio <143807170+YagoAkio@users.noreply.github.com>
2024-06-13 07:31:58 -0300

    nao da merge

---
+++ santa-casa-frontend/Funcao_Saida/relatorioEntrada/relatorioEntradaCtrl.js
@@ -0,0 +1,104 @@
+const urlBase = "http://localhost:4040"
+
+function formataData(dataParametro) {
+    // Convertendo a string em um objeto Date
+    let data = new Date(dataParametro);
+
+    // Obtendo o ano, mês e dia
+    let ano = data.getFullYear();
+    let mes = ('0' + (data.getMonth() + 1)).slice(-2); // Adicionando 1 porque os meses são zero indexados
+    let dia = ('0' + data.getDate()).slice(-2);
+
+    // Formatando a data no formato esperado
+    let dataFormatada = ano + '-' + mes + '-' + dia;
+
+    // Retornando a data formatada
+    return dataFormatada;
+}
+
+const butoes = {
+    data() {
+        return {
+            isListar: false,
+            isFuncionario: false,
+            isEntrada: false,
+            todasEntradas: [],
+            msm: ''
+        }
+    },
+    methods: {
+        exibir(aux) {
+            if (aux === 'Listar') {
+                this.isListar = true;
+                this.isFuncionario = false;
+                this.isEntrada = false;
+                this.msm = "RELATÓRIO DE ENTRADA"
+                this.gerarLista();
+            } else if (aux === 'Funcionario') {
+                this.isListar = false;
+                this.isFuncionario = true;
+                this.isEntrada = false;
+                this.msm = "RELATÓRIO DE FUNCIONÁRIO"
+            } else {
+                this.isListar = false;
+                this.isFuncionario = false;
+                this.isEntrada = true;
+                this.msm = "RELATÓRIO DE ENTRADA POR PRODURO"
+            }
+        },
+        gerarLista() {
+            axios.get(urlBase + '/entrada').then((response) => {
+                this.todasEntradas = response.data.listaEntradas;
+                console.log(this.todasEntradas);
+            })
+        },
+        excluir(aux) {
+            axios.delete(urlBase + '/entrada/' + aux).then(
+                () => {
+                    this.todasEntradas = [];
+                    this.gerarLista();
+                }
+            )
+        },
+        formataData(dataParametro) {
+            return formataData(dataParametro);
+        }
+    },
+    mounted() {
+
+    },
+    template: `
+    <div class="div-opcoes-filtro">
+        <button class="botoes-exibicao" @click="exibir('Listar')">Listar consumos</button>
+        <button class="botoes-exibicao" @click="exibir('Funcionario')">Relatório Consumo por Paciente</button>
+        <button class="botoes-exibicao" @click="exibir('Entrada')">Relatório Consumo de Produto</button>
+    </div>
+    <h1 id="titulo">{{msm}}</h1>
+    <div>
+        <table class="table table-striped table-hover" v-if="todasEntradas.length > 0">
+            <thead v-if="todasEntradas.length > 0">
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nome Funcionario</th>
+                <th scope="col">CPF DO Funcionario</th>
+                <th scope="col">Data Entrada</th>
+                <th scope="col">Itens Entrada</th>
+                <th scope="col">Ações</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr v-for="c in todasEntradas" :key="c.entrada_id">
+                    <td>{{c.entrada_id}}</td>
+                    <td>{{c.funcionario.nome_funcionario}}</td>
+                    <td>{{c.funcionario.cpf}}</td>
+                    <td>{{formataData(c.data_entrada)}}</td>
+                    <td><button @click="listarItens(c.itensEntrada)">+</button></td>
+                    <td v-if="new Date() - new Date(c.data_entrada) <= 86400000"><button @click="excluir(c.entrada_id)">-</button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `
+}
+
+Vue.createApp(butoes).mount('#APP')
