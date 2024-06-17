0d1b6b610f726b7ad41c84fef1621c87bd1fc452
LeonRonchi <leonbronchi@gmail.com>
2024-06-17 15:20:41 -0300

    transferência

--- santa-casa-backend/persistencia/loteDAO.js
+++ santa-casa-backend/persistencia/loteDAO.js
@@ -10,6 +10,7 @@ export default class LoteDAO{
     //da para cadastrar lote e codigo iguais tenho que consertar
     async gravar(lote,conexao){
         if(lote instanceof Lote){
+            lote.local.loc_id = parseInt(lote.local.loc_id);
             const sql = `INSERT INTO lote(codigo,
                         data_validade,
                         quantidade,
@@ -74,12 +75,66 @@ export default class LoteDAO{
         }
     }
 
+    async atualizar2(lote, conexao){
+        if(lote instanceof Lote) { 
+            const sql= `UPDATE lote SET quantidade = ?, total_conteudo = ? WHERE codigo = ? AND produto_prod_ID = ? AND loc = ?;`;
+            const parametros = [
+                lote.quantidade,
+                lote.total_conteudo,
+                lote.codigo,
+                lote.produto.prod_ID,
+                lote.local.loc_id
+            ];
+            await conexao.execute(sql, parametros);
+        }
+    }
+
     async excluir(lote,conexao){
         if(lote instanceof Lote){
-            const sql = "DELETE FROM lote WHERE codigo = ? AND produto_prod_ID = ?";
+            const sql = "DELETE FROM lote WHERE codigo = ? AND produto_prod_ID = ?;";
             const parametros =[lote.codigo, lote.produto.prod_ID];
             await conexao.execute(sql, parametros);
         }
+    }
+
+    async consultar2(lote){
+        let sql = "";
+        let parametros = [];
+
+        sql = `SELECT * FROM 
+        lote WHERE  produto_prod_ID = ? AND codigo = ? AND loc = ?;`
+        parametros = [lote.produto.prod_ID,lote.codigo,parseInt(lote.local.loc_id)];
+
+        const conexao = await conectar();
+        const [registros, campos]= await conexao.execute(sql, parametros);//A execução do select retornará uma lista dos registros que atendem a condição em "registros"
+        let listaLotes = [];
+        //Preenchendo a lista com cada registro retornado
+        for(const registro of registros){
+            let produto = new Produto(registro.produto_prod_ID);
+            await produto.consultar(registro.produto_prod_ID).then((listaProdutos)=>{
+                produto = listaProdutos.pop();
+            });
+            let formaFarmaceutica = new FormaFarmaceutica(registro.formafarmaceutica_ffa_cod);
+            await formaFarmaceutica.consultar(registro.formafarmaceutica_ffa_cod).then((listaFormaFaramaceuticas)=>{
+                formaFarmaceutica = listaFormaFaramaceuticas.pop();
+            });
+            let unidade = new Unidade(registro.unidade_un_cod);
+            await unidade.consultar(registro.unidade_un_cod).then((listaUnidades)=>{
+                unidade = listaUnidades.pop();
+            });
+            let local = new Local(registro.local_loc_id);
+            await local.consultar(registro.local_loc_id).then((listaLocais)=>{
+                local = listaLocais.pop();
+            });
+            
+            let lote = new Lote(registro.codigo,registro.data_validade,
+                registro.quantidade,produto,formaFarmaceutica,registro.conteudo_frasco,
+                unidade,registro.total_conteudo,local,registro.data_entrada
+            );
+            listaLotes.push(lote);
+        }
+        global.poolConexoes.releaseConnection(conexao);
+        return listaLotes;
     }
 
     async consultar(lote){
@@ -87,7 +142,7 @@ export default class LoteDAO{
         let parametros = [];
         if(lote.produto != null){
             sql = `SELECT * FROM 
-            lote WHERE  produto_prod_ID = ?`
+            lote WHERE produto_prod_ID = ?`
             if(lote.codigo!=0){
                 sql  += ` AND codigo = ?;`
                 parametros = [lote.produto.prod_ID,lote.codigo];
